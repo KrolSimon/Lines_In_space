9d750172546755ceb16c4ab1fd4be2aa4a2ea6db
KrolSilmon <60669254+KrolSilmon@users.noreply.github.com>
2022-10-27 10:45:09 +0100

    Minor changes to p5 project

--- P5JS/sketch.js
+++ P5JS/sketch.js
@@ -10,7 +10,7 @@ let c = 8.0/3.0;
 
 
 
-console.log(v);
+
 function setup() {
   createCanvas(400, 400, WEBGL);
   background(0);
@@ -18,7 +18,8 @@ function setup() {
 }
 
 function draw() {
-
+  let i = 0;
+  i = i + 1;
   let dt = 0.01;
   let dx = (a * (y - x)) * dt;
   let dy = (x * (b - z) - y) * dt;
@@ -27,12 +28,10 @@ function draw() {
   y = y + dy;
   z = z + dz;
 
- 
 
- 
-  scale(5);
   stroke(255);
 
+  
 
   point(x,y,z);
 
